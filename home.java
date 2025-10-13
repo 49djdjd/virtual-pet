1ce519595c46bc35b2457fb7fdda21774a8b2393
Azra Ali <azra_ali09@outlook.com>
2025-10-12 21:10:26 -0400

    wrote the basic variables/methods working on interface/front-end

---
+++ home.java
@@ -0,0 +1,5 @@
+//first time using this so referenced ai on what to import
+import javax.swing.Jframe;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
---
+++ pet.java
@@ -0,0 +1,68 @@
+public class PetClass{
+    private String name;
+    private int hunger;
+    private int energy;
+    private int happiness;
+    private boolean alive;
+    public PetClass(String a, int b, int c, int d){
+        name = a;
+        hunger = b;
+        energy = c;
+        happiness = d;
+        alive = true;
+
+    }
+    public String getName(){
+        return name;
+    }
+    public int getHunger(){
+        return hunger;
+    }
+    public int getEnergy(){
+        return energy;
+    }
+    public int getHappiness(){
+        return happiness;
+    }
+    public void feedPet(){
+        hunger -= 10;
+    }
+
+    public void playTime(){
+        happiness += 20;
+        energy -= 10;
+    }
+    public void restPet(){
+        energy += 25;
+    }
+    public boolean petAlive(){
+        if (happiness <=0 && hunger == 100){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    public void statusBar(){
+        System.out.println("Pet name: " + name)
+        System.out.println("Pet hunger: " + hunger )
+        System.out.println("Pet energy: " + energy)
+        System.out.println("Pet happiness: " + happiness )
+    }
+    public void makeEverythingIsPositive(){
+        if (happiness<0){
+            happiness = 0;
+        }
+        if (energy<0){
+            energy=0;
+        }
+        if (hunger<0){
+            hunger = 0;
+        }
+    }
+    public void passTime(){
+        hunger += 5;
+        energy -= 2;
+        happines -=5;
+    }
+}
