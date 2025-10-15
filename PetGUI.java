42c97ef070edc6141121d76356147477c4f2f08a
Azra Ali <azra_ali09@outlook.com>
2025-10-15 19:31:36 -0400

    figured out how to run it and have the image show up and fixed the dimnesions of the frame

--- PetGUI.java
+++ PetGUI.java
@@ -1,25 +1,32 @@
 //first time using this so referenced ai on what to import
-import javax.swing.Jframe;
+import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
+import javax.swing.SwingUtilities;
 
-//referenced online sites and AI for this part 
+//referenced online sites and AI for this part
 public class PetGUI{
     public PetGUI(){
         //creating new object
         JFrame frame = new JFrame("My Virtual Pet");
-        frame.setSize(500,400);
+        frame.setSize(1500,1000);
         //creaing image icon
         ImageIcon pet = new ImageIcon("pet.png");
         //holding image
-        JLabel petLabel = new JLabel();
-        frame.add(petLabel)
+        JLabel petLabel = new JLabel(pet);
+        frame.add(petLabel);
+        frame.setVisible(true);
 
     }
-}
 public static void main(String[] args){
-    pubilic void run(){
-        new PetGUI();
-    }
+    SwingUtilities.invokeLater((new Runnable() {
+        @Override
+        public void run(){
+            new PetGUI();
+        }
+
+    }));
+
+}
 }
