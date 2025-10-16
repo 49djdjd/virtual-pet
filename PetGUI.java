cf9e41460be3c335436d25b661ebe7c7985e199c
Azra Ali <azra_ali09@outlook.com>
2025-10-15 20:07:27 -0400

    created the buttons and was able to have everything be visible

--- PetGUI.java
+++ PetGUI.java
@@ -1,4 +1,8 @@
 //first time using this so referenced ai on what to import
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -8,17 +12,33 @@ import javax.swing.SwingUtilities;
 //referenced online sites and AI for this part
 public class PetGUI{
     public PetGUI(){
+
         //creating new object
+        PetClass pet = new PetClass("silly", 100,100,100);
         JFrame frame = new JFrame("My Virtual Pet");
+        frame.setLayout(new FlowLayout());
         frame.setSize(1500,1000);
         //creaing image icon
-        ImageIcon pet = new ImageIcon("pet.png");
+        ImageIcon petImage = new ImageIcon("pet.png");
         //holding image
-        JLabel petLabel = new JLabel(pet);
+        JLabel petLabel = new JLabel(petImage);
         frame.add(petLabel);
+        JPanel statusbar = new JPanel();
+        JButton feedButton = new JButton("Feed Pet:");
+        JButton playButton = new JButton("Play with Pet");
+        //this adds them to window
+        frame.add(feedButton);
+        frame.add(playButton);
+        //hmm still kind of confused of this section
+        feedButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                pet.feedPet();
+                System.out.println("Pet is fed!");
+            }
+            });
         frame.setVisible(true);
-
-    }
+        }
 public static void main(String[] args){
     SwingUtilities.invokeLater((new Runnable() {
         @Override
