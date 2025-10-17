b1bed3ac72b725b826825b2f947e56c0a428e2dc
Azra Ali <azra_ali09@outlook.com>
2025-10-17 17:29:03 -0400

    had an issue with the status but finished debugging and organized the status code a little

--- PetGUI.java
+++ PetGUI.java
@@ -2,15 +2,23 @@
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+//groups things togetehr
 import javax.swing.JPanel;
+//main window
 import javax.swing.JFrame;
+//clickable button
 import javax.swing.JButton;
+//displays text/image
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import javax.swing.SwingUtilities;
 
 //referenced online sites and AI for this part
 public class PetGUI{
+    private JLabel nameStatus;
+    private JLabel hungerStatus;
+    private JLabel happinessStatus;
+    private JLabel energyStatus;
     public PetGUI(){
 
         //creating new object
@@ -22,8 +30,21 @@ public class PetGUI{
         ImageIcon petImage = new ImageIcon("pet.png");
         //holding image
         JLabel petLabel = new JLabel(petImage);
+        frame.setLayout(new FlowLayout());
         frame.add(petLabel);
-        JPanel statusbar = new JPanel();
+        JPanel status = new JPanel();
+        frame.add(status);
+        nameStatus = new JLabel();
+        hungerStatus = new JLabel();
+        energyStatus = new JLabel();
+        happinessStatus = new JLabel();
+        status.add(nameStatus);
+        nameStatus.setText(null);
+        status.add(hungerStatus);
+        hungerStatus.setText("Hunger: " + pet.getHunger());
+        status.add(energyStatus);
+        status.add(happinessStatus);
+        status.add(new JLabel("Pet Status "));
         JButton feedButton = new JButton("Feed Pet:");
         JButton playButton = new JButton("Play with Pet");
         //this adds them to window
@@ -34,6 +55,7 @@ public class PetGUI{
             @Override
             public void actionPerformed(ActionEvent e){
                 pet.feedPet();
+                
                 System.out.println("Pet is fed!");
             }
             });
