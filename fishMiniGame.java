f78943f8f4c1c38b26bdb024d2f9d9d631a77f84
Azra Ali <azra_ali09@outlook.com>
2025-10-18 22:31:10 -0400

    having issues with the interection of the pet and fish images

--- fishMiniGame.java
+++ fishMiniGame.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -29,7 +30,6 @@ public class fishMiniGame extends JPanel{
     private JLabel[] fishArray;
     private final int numFish = 5;
     public fishMiniGame(JFrame b){
-
         JPanel gamePanel = new JPanel();
         frame = b;
         //referenced ai to help delete everything for new panel
@@ -42,7 +42,6 @@ public class fishMiniGame extends JPanel{
         gamePanel.setLayout(null);
         petLabel = new JLabel(new ImageIcon("pet.png"));
         petLabel.setBounds(200,500,500,500);
-
         gamePanel.add(petLabel);
         ImageIcon fish = new ImageIcon("fish.png");
         //ai used to help with array (so multiple fish can come down together)
@@ -51,7 +50,7 @@ public class fishMiniGame extends JPanel{
             JLabel fishLabel = new JLabel(fish);
             int maximum = Math.max(1, gamePanel.getWidth() - 200);
             int random = (int)(Math.random()*maximum);
-            fishLabel.setBounds(random,0, 200,200);
+            fishLabel.setBounds(random,0, 500,500);
             fishArray[i] = fishLabel;
             gamePanel.add(fishLabel);
     
@@ -60,6 +59,11 @@ public class fishMiniGame extends JPanel{
         Timer fishTimer = new Timer(20, e-> {
             for (int i = 0; i < fishArray.length; i++){
                 fishArray[i].setLocation(fishArray[i].getX(), fishArray[i].getY()+5);
+                Rectangle petRect = petLabel.getBounds();
+                Rectangle fishRect = fishArray[i].getBounds();
+                if (petRect.intersects(fishRect)){
+                    System.out.println("+1 Hunger!");
+                }
             }
         });
         fishTimer.start();
